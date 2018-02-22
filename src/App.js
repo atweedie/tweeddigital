4b3cfd490c1fc6e0994cfc4b49403a75c755a5c6
Adam Tweedie <adam_tweedie@msn.com>
2018-02-22 18:58:46 +0000

    -- Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Splash from './components/Splash'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Splash/>
+      </div>
+    );
+  }
+}
+
+export default App;
